a04351e2d8674baf02399116ade668d85e4c1caa
dineshmm23 <dineshmm23@gmail.com>
2021-03-25 21:56:41 +0530

    Fixed

--- scripts/build_ng.js
+++ scripts/build_ng.js
@@ -3,7 +3,7 @@
 
 const { execSync } = require("child_process");
 
-const PROJECT_DIR = "./pavan";
+const PROJECT_DIR = "./recaptcha-web";
 
 module.exports = function (context) {
   execSync(
@@ -21,7 +21,7 @@ module.exports = function (context) {
 
 
   execSync(
-    "ng build --aot=false --build-optimizer=false --output-path ../www",
+    "ng build --aot=false --build-optimizer=false --output-path ../platforms/ios/www",
     { cwd: PROJECT_DIR, stdio: [0, 1, 2] },
     (err, stdout, stderr) => {
       console.log("ng build potentially failed.");
